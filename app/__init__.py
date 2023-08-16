6d993a131549cdd60cf872c9d7b1a0e7c2c4951e
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-08-16 16:25:13 +0300

    Adding Flask migration, add user type to User table

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 
 from config import DevelopmentConfig
-from app.extensions import db, login_manager, jwt
+from app.extensions import db, login_manager, jwt, migrate
 from app.main import bp as main_bp
 from app.authentication import bp as auth_bp
 from app.track import bp as track_bp
@@ -12,19 +11,19 @@ from app.track import bp as track_bp
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app, supports_credentials=True, origins=['http://localhost:4200'])  # todo - remove before deployment
 
-    # Initialize Flask extensions here
+    # Handle CORS # todo - remove before deployment
+    CORS(app, supports_credentials=True, origins=['http://localhost:4200'])
+
+    # Initialize Flask extensions
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     jwt.init_app(app)
 
-    # Register blueprints here
+    # Register blueprints
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(track_bp)
 
     return app
-
--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,11 @@
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+
+migrate = Migrate()
 
 login_manager = LoginManager()
 
