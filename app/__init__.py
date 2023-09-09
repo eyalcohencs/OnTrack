17a0258235202bb529f01c80c32a1691360fe6cd
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-09-10 00:01:30 +0300

    auto migrations

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ def create_app(config_class=DevelopmentConfig):
     from alembic.config import Config
     from alembic import command
 
-    alembic_cfg = Config("alembic/alembic.ini")  # Path to your Alembic configuration file
+    alembic_cfg = Config("../migrations/alembic.ini")  # Path to your Alembic configuration file
     command.upgrade(alembic_cfg, "head")
 
     return app
