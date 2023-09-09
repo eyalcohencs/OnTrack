0d1ae3d5bf608ba678b68ed08e473d5a3c77b7f3
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-09-10 00:24:18 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from alembic.config import Config
 from alembic import command
 
 app = create_app()
-# Run migrations
-
-alembic_cfg = Config("migrations/alembic.ini")  # Path to your Alembic configuration file
-command.upgrade(alembic_cfg, "head")
+with app.app_context():
+    # Run migrations
+    alembic_cfg = Config("migrations/alembic.ini")  # Path to your Alembic configuration file
+    command.upgrade(alembic_cfg, "head")
