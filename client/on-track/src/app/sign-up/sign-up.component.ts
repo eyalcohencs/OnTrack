a0e4e6ac7c5b29860486188c3731f439a0e04da9
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-09-12 01:33:08 +0300

    Update login and registration forms
    Adding Font Awesome, validation to forms input and UI

--- client/on-track/src/app/sign-up/sign-up.component.ts
+++ client/on-track/src/app/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { ApiService } from '../services/api-service/api-service.service';
 import { User } from '../services/user-enum';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -11,8 +13,23 @@ import { Router } from '@angular/router';
 export class SignUpComponent {
   constructor(
     private apiService: ApiService,
-    private router: Router
-    ) {}
+    private router: Router,
+    private formGroup: FormBuilder
+    ) {
+        this.registrationForm = this.formGroup.group(
+          {
+            first_name: ['', [Validators.required, Validators.minLength(1)]],
+            last_name: ['', [Validators.required, Validators.minLength(1)]],
+            email: ['', [Validators.required, Validators.email, Validators.minLength(1)]],
+            username: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^[a-zA-Z]+$/)]],
+            password: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$/)]],
+          }
+        );
+    }
+  
+  registerFailedWarning: boolean = false;
+
+  registrationForm: FormGroup;
   
   user: User = {
     first_name: null,
@@ -24,14 +41,11 @@ export class SignUpComponent {
 
   async register() {
     try {
-      // TODO - Validate fields
       const result: any = await this.apiService.register(this.user);
-      console.log('register component' + result);
       this.router.navigate(['/login'])
-
     } catch(e) {
       console.log(e)
-
+      this.registerFailedWarning = true;
     }
   }
 
