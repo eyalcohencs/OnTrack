401b12cf5104a62bc250079ba061206ee8c6a9a6
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-09-26 16:34:52 +0300

    make update graph more efficient with less db hits, less calculations and high performance

--- app/track/graph_service.py
+++ app/track/graph_service.py
@@ -1,18 +1,17 @@
 from app.track.db_service import Neo4jDB
 from app.track.graph_logic import is_there_already_a_close_point_in_the_graph, distance_between_points, \
-    are_the_same_point_by_coordinates
+    are_the_same_point_by_coordinates, add_point_to_group_of_points
 
 graph_db = Neo4jDB()
 
 
-def add_point_to_graph(point_to_add, all_points=None):
-    points = all_points if all_points else graph_db.get_all_points_from_db()
-    collided_point = is_there_already_a_close_point_in_the_graph(point_to_add, points)
+def add_point_to_graph(point_to_add, grouped_points, grouped_key_function):
+    collided_point = is_there_already_a_close_point_in_the_graph(point_to_add, grouped_points, grouped_key_function)
     if collided_point:
         point_to_add = collided_point
     else:
         point_to_add = graph_db.add_point_to_db(point_to_add)
-        all_points.append(point_to_add)
+        add_point_to_group_of_points(grouped_points, point_to_add, grouped_key_function)
     return point_to_add
 
 
@@ -22,17 +21,16 @@ def add_edge_to_graph(source_point, target_point, data=None):
         graph_db.add_edge_to_db(source_point, target_point, data)
 
 
-def add_point_and_relation_to_exist_point_to_graph(existed_point, point_to_add, data=None, all_points=None):
+def add_point_and_relation_to_exist_point_to_graph(existed_point, point_to_add, data=None, grouped_all_points=None, grouping_creation_key=None):
     data = data if data is not None else {'track_id': None}
-    points = all_points if all_points else graph_db.get_all_points_from_db()
-    collided_point = is_there_already_a_close_point_in_the_graph(point_to_add, points)
+    collided_point = is_there_already_a_close_point_in_the_graph(point_to_add, grouped_points=grouped_all_points, create_key=grouping_creation_key)
     if collided_point:
         point_to_add = collided_point
         if not are_the_same_point_by_coordinates(existed_point, point_to_add):
             graph_db.add_edge_to_db(existed_point, point_to_add, data)
     else:
-        graph_db.add_point_and_relation_to_exist_point_db(existed_point, point_to_add, data)
-        all_points.append(point_to_add)
+        point_to_add = graph_db.add_point_and_relation_to_exist_point_db(existed_point, point_to_add, data)
+        add_point_to_group_of_points(grouped_all_points, point_to_add, grouping_creation_key)
     return point_to_add
 
 
--- app/track/utils.py
+++ app/track/utils.py
@@ -1,8 +1,7 @@
-# import pandas as pd
+import os
 
 from app.track.graph_logic import are_the_same_point_by_coordinates, are_two_points_too_close
 
-GAP_BETWEEN_POINTS_IN_METERS_FOR_REDUCTION = 50
 CALCULATED_ROAD = 'CALCULATED_ROAD'
 
 
@@ -51,30 +50,9 @@ class GeoRoad:
         return f'{self.uuid} | {self.track_id}'
 
 
-# def gpx2df(gpx):
-#     data = gpx.tracks[0].segments[0].points
-#
-#     df = pd.DataFrame(columns=['longitude', 'latitude', 'altitude', 'time'])
-#     for point in data:
-#         df = pd.concat([df, pd.DataFrame([
-#             {'longitude': point.longitude,
-#              'latitude': point.latitude,
-#              'altitude': point.altitude,
-#              'time': point.time}])
-#                         ], ignore_index=True)
-#
-#     # df['time'] = df['time'].astype(str)
-#     df['time'] = pd.to_datetime(df['time'], dayfirst=True)
-#     return df
-
-
-def extract_points_of_gpx_track(gpx_track_data):
-    gpx_points = gpx_track_data.tracks[0].segments[0].points
-    # Todo - This is the place to add additional data like track difficulty
-    return convert_gpx_to_geo_point_list(gpx_points)
-
-
+# todo - check if the function is in the right file, maybe graph_logic
 def reduce_points_in_track_based_on_distance(geo_points):
+    gap_between_points_in_meters_for_reduction = os.environ['GAP_BETWEEN_POINTS_IN_METERS_FOR_REDUCTION']
     anchor_index = 0
     index = 1
     reduced_points = [geo_points[anchor_index]]
@@ -85,20 +63,12 @@ def reduce_points_in_track_based_on_distance(geo_points):
         # Check if they are same is little faster than calculate distance
         if not are_the_same_point_by_coordinates(anchor_point, checked_point) \
                 and not are_two_points_too_close(
-                anchor_point, checked_point, GAP_BETWEEN_POINTS_IN_METERS_FOR_REDUCTION):
+                anchor_point, checked_point, int(gap_between_points_in_meters_for_reduction)):
             reduced_points.append(checked_point)
             anchor_index = index
         index = index + 1
 
     return reduced_points
-
-
-# def convert_gpx_to_geo_point_list(points_to_convert):
-#     geo_points = []
-#     for point in points_to_convert:
-#         altitude = point.elevation if hasattr(point, 'elevation') else point.altitude
-#         geo_points.append(GeoPoint(point.longitude, point.latitude, altitude, point.time, uuid=None))
-#     return geo_points
 
 
 def convert_coordinates_list_to_geo_point_list(coords_list):
