470c32c4c9992a862776d12da71b31010f93c045
eyalcohencs <57841654+eyalcohencs@users.noreply.github.com>
2023-09-29 00:31:56 +0300

    Move update_graph_service back to monolithic app

--- app/config.py
+++ app/config.py
@@ -15,8 +15,8 @@ class Config:
     MAIL_PORT = 465
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     CACHE_TYPE = 'simple'  # 'redis'
     CACHE_DEFAULT_TIMEOUT = 300
     CACHE_REDIS_URL = os.environ.get('CACHE_REDIS_URL')
